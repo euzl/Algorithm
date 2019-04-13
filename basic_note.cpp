11d374c58bad8f8f5f0d1f8a9bc25e3665584fe1
Yujin Lee <37680108+euzl@users.noreply.github.com>
2019-04-14 01:24:14 +0900

    Update basic_note.cpp

--- basic_note.cpp
+++ basic_note.cpp
@@ -1,16 +1,16 @@
 /* 문제풀이하면서 필요한 기초개념들 기록 - C++ */
 
-/* 최댓값/최솟값 */
+@최댓값/최솟값
 #include <algorithm>
 max(int num1, int num2, ...); //인자 중 최대값 반환. 변수 못넣음. 
 min(int num1, int num2, ...); // 최소값
 
-/* 절댓값 */
+@절댓값
 #include <cstdlib> // int 형인 경우
 #include <cmath.h> // double, float
 abs(int num); // 절대값 반환 (absolute value)
 
-/* \ 역슬래쉬 사용 */
+@\ : 역슬래쉬 사용
 /*
 \t  : tab 효과
 \n  : enter 효과(endl)
@@ -18,7 +18,7 @@ abs(int num); // 절대값 반환 (absolute value)
 \\  : \ 출력 (하나만 하면 출력X)
 */
 
-/* 문자열 입력 */
+@문자열 입력
 #include <iostream>
 char str_input[10];
 char k;
