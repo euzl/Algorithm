86290e90a0f2f68e68d9898060fcf5e1b13e416e
Yujin Lee <37680108+euzl@users.noreply.github.com>
2019-04-14 00:53:25 +0900

    Update basic_note.cpp

--- basic_note.cpp
+++ basic_note.cpp
@@ -9,3 +9,11 @@ min(int num1, int num2, ...); // 최소값
 #include <cstdlib> // int 형인 경우
 #include <cmath.h> // double, float
 abs(int num); // 절대값 반환 (absolute value)
+
+/* \ 역슬래쉬 사용 */
+/*
+\t  : tab 효과
+\n  : enter 효과(endl)
+\', \"  : 따옴표(', ") 표시
+\\  : \ 출력 (하나만 하면 출력X)
+*/
