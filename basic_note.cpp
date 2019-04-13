b59415d08ff56cbd20772c93b038921d72db8762
Yujin Lee <37680108+euzl@users.noreply.github.com>
2019-04-14 01:22:53 +0900

    Update basic_note.cpp

--- basic_note.cpp
+++ basic_note.cpp
@@ -17,3 +17,19 @@ abs(int num); // 절대값 반환 (absolute value)
 \', \"  : 따옴표(', ") 표시
 \\  : \ 출력 (하나만 하면 출력X)
 */
+
+/* 문자열 입력 */
+#include <iostream>
+char str_input[10];
+char k;
+cin >> str_input; // 공백, 줄바꿈 기준으로 입력받음
+cin.get(k); // 문자 입력받음
+
+cin.getline(str_input, 10); 
+// 줄바꿈 기준으로 입력받음 ==> *공백도 문자로 간주!
+// 파라미터:(문자열을 저장할 배열이름, 입력받을 최대 문자 개수)
+
+// string은 이렇게!
+#include <string>
+string str_input;
+getline(cin, str_input); // 두번째 파라미터에 입력받은 스트링을 저장할 이름 
