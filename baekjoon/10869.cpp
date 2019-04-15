35ea3313189dc5787a490f3df3687678b1202512
Yujin Lee <37680108+euzl@users.noreply.github.com>
2019-04-16 03:09:06 +0900

    2019/04/16 Solved the problem

---
+++ baekjoon/10869.cpp
@@ -0,0 +1,9 @@
+// 10869::사칙연산
+#include <iostream>
+using namespace std;
+int main(int a, int b) {
+	cin >> a >> b;
+	cout << a + b << endl << a - b << endl << a * b << endl << a / b << endl << a%b;
+	
+	return 0;
+}
