1ce13636f31aa84d34938806b7c40edd3f2256f2
Yujin Lee <37680108+euzl@users.noreply.github.com>
2019-06-07 23:04:23 +0900

    2019/06/07 Solved the problem

---
+++ baekjoon/10430.cpp
@@ -0,0 +1,7 @@
+// 10430::나머지
+#include <iostream>
+using namespace std;
+int main(int a, int b, int c) {
+	cin >> a >> b >> c;
+	cout << (a + b) % c << "\n" << (a%c + b % c) % c << "\n" << (a*b) % c << "\n" << (a%c*b%c) % c;
+}
