9046ce4b4f1d024f62ce6644b719425b5bc8064c
Yujin Lee <37680108+euzl@users.noreply.github.com>
2019-06-12 00:03:27 +0900

    2019/06/12 Solved the problem

---
+++ baekjoon/2438.cpp
@@ -0,0 +1,13 @@
+// 2438::별 찍기 -1
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 1; i <= a; i++) {
+		for (int j = 1; j <= i; j++)
+			cout << '*';
+		cout << endl;
+	}
+	return 0;
+}
