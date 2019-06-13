9b6ca66b064020a25397e95b17b5ebcd9a10cbc3
Yujin Lee (이유진) <euzl@khu.ac.kr>
2019-06-14 01:23:20 +0900

    2019/06/14 Solved the problem

---
+++ baekjoon/2440.cpp
@@ -0,0 +1,13 @@
+// 2440::별 찍기 - 3
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	for (int i = a; i >= 1; i--) {
+		for (int j = 1; j <= i; j++)
+			cout << '*';
+		cout << endl;
+	}
+	return 0;
+}
