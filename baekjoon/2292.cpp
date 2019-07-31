fddc9dbc4f7e35e2ceb7614cf63b0e3b1cb1aa39
Yujin Lee (이유진) <euzl@khu.ac.kr>
2019-07-31 11:29:59 +0900

    2019/07/31 Solved the problem

--- baekjoon/2292.cpp
+++ baekjoon/2292.cpp
@@ -3,13 +3,8 @@
 
 int main() {
 	int n, i = 0;
-	scanf_s("%d", &n);
-	while (1) {
-		if ((3 * i * (i + 1) + 1) >= n) {
-			printf("%d", i + 1);
-			break;
-		}
-		i++;
-	}
+	scanf("%d", &n);
+	for (i; (3 * i * (i + 1) + 1) < n; i++);
+	printf("%d", i + 1);
 	return 0;
 }
