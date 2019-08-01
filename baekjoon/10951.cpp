a74fbd7c6ea4534bb5356a3e094ae49b464d8ded
Yujin Lee (이유진) <euzl@khu.ac.kr>
2019-08-02 00:56:42 +0900

    2019/08/02 Solved the problem

--- baekjoon/10951.cpp
+++ baekjoon/10951.cpp
@@ -1,3 +1,9 @@
 // 10951::A+B -4
-#include <stdio.h>
-main() {int a, b;while(scanf("%d%d", &a, &b))printf("%d\n", a + b);}
+#include <cstdio>
+
+int main() {
+	int a, b;
+	while (scanf_s("%d %d", &a, &b) != EOF)
+		printf("%d\n", a + b);
+	return 0;
+}
