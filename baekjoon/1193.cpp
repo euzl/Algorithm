3fecb6e208c61cd0a41373c51a6e4c7b570d37bb
Yujin Lee (이유진) <euzl@khu.ac.kr>
2019-08-04 00:19:14 +0900

    2019/08/04 Solved the problem

--- baekjoon/1193.cpp
+++ baekjoon/1193.cpp
@@ -1,15 +1,16 @@
-// 1193::분수찾기 (실패)
+// 1193::분수찾기
 #include <cstdio>
 
 int main() {
 	int x, i = 0;
 	scanf("%d", &x);
-	while (x < i * (i + 1) / 2) {
+	while (x > i * (i + 1) / 2) { // 몇 번째 i에 속하는지 찾기
 		i++;
-	}
-	for (int j = 0; j < i; j++) {
-		if (i - j == x) {
-			printf("%d/%d", 1 + j, i - j);
+	} 
+	//printf("# x : %d | i : %d\n", x, i);
+	for (int j = 0; j < i; j++) { // i안에서 몇 번째인지 찾기
+		if (i * (i + 1) / 2 - j == x) { // x를 찾으면?
+			i % 2 != 0 ? printf("%d/%d\n", 1 + j, i - j) : printf("%d/%d\n", i - j, 1 + j);
 			break;
 		}
 	}
