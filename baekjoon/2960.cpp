e8236e961eb685adc026667460056bc16cfb4601
Yujin Lee (이유진) <euzl@khu.ac.kr>
2019-08-16 02:27:13 +0900

    2019/08/16 Incorrect

---
+++ baekjoon/2960.cpp
@@ -0,0 +1,30 @@
+// 2960 :: 에라토스테네스의 체
+// 틀렸는데, 내가 실행했을 때는 잘 되고 뭐가 틀린지 모르겠다 ㅠㅠㅠㅠ
+#include <cstdio>
+
+int main()
+{
+	int n, k, cnt = 0;
+	bool num[1001] = { false, };
+	scanf("%d%d", &n, &k);
+	for (int i = 2; i <= n; i++) // (1)2부터 n까지 모든 정수를 적는다
+		num[i] = true;
+	for (int i = 2; i < n; i++) { // (2) 아직 지우지 않은 수 중 가장 작은 수를 찾는다. <= i (이것을 p라고 하고, 이 수는 소수이다.)
+		if (num[i] == false) 
+			continue;
+		for (int j = 1; j < n; j++) { // (3) p를 지우고, 아직 지우지 않은 p의 배수를 크기 순서대로 지운다.
+			if (i * j > n)
+				break;
+			if (num[i * j] == false)
+				continue;
+			cnt++;
+			if (cnt == k) { // k번째 지우는 수
+				printf("%d", i * j);
+				return 0;
+			}
+			num[i * j] = false;
+		} // (4) 아직 모든 수를 지우지 않았다면, 다시 2번 단계로 간다.
+	}
+
+	return 0;
+}
