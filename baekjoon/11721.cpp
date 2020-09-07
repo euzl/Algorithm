f80bb70f62a85014a61b24c60775ce87d5be97ef
new Dean("유진") <euzl@khu.ac.kr>
2020-09-08 03:25:14 +0900

    2020/09/08 열 개씩 끊어 출력하기

--- baekjoon/11721.cpp
+++ baekjoon/11721.cpp
@@ -1,18 +1,21 @@
 // 11721::열 개씩 끊어 출력하기
-// 최선을 다했지만 실패 ㅠㅠ 나중에 다시 시도
 #include <iostream>
-#include <string.h>
 using namespace std;
+
 int main() {
-	string input;
-	getline(cin, input);
-	char* input_arr = new char[input.size() + 1];
-	strcpy(input_arr, input.size() + 1, input.c_str());
-	for (int i = 0; i < input.size() + 1; i++) {
-		printf("%c",input_arr[i]);
-		if ((i+1) % 10 == 0)
-			printf("\n");
-	}
-	delete[] input_arr;
-	return 0;
+    int a = 0;
+    char input[101];
+    cin.getline(input ,101);
+
+    while (a < 101) {
+        if (input[a] == '\0') {
+            return 0;
+        }
+        cout<<input[a];
+        a++;
+        if(a%10 == 0) {
+            cout<<endl;
+        }
+    }
+    return 0;
 }
