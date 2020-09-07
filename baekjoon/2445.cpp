47ecf366a010e461392fb7d098f5eb0b7a3edf1e
new Dean("유진") <euzl@khu.ac.kr>
2020-09-08 03:46:19 +0900

    2020/09/08 별 찍기 - 8

---
+++ baekjoon/2445.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int k = t-1; k >= 1-t; k--) {
+        for (int i=0; i<t-abs(k); i++) {
+            cout << '*';
+        }
+        for (int j=0; j<2*abs(k); j++) {
+            cout << ' ';
+        }
+        for (int i=0; i<t-abs(k); i++) {
+            cout << '*';
+        }
+        if (k > 1-t) cout << endl;
+    }
+    return 0;
+}
