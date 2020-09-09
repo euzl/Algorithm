3f680195bc0de76ad1e1812d75c51b3efcea6018
new Dean("유진") <euzl@khu.ac.kr>
2020-09-10 06:07:17 +0900

    2020/09/10 가장 긴 바이토닉 부분 수열

---
+++ baekjoon/11054.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int getMax(int a, int b) {
+    return a>b? a : b;
+}
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+
+    int arr[1001];
+    int length[2][1001] = {0, };
+    int max = -1;
+    for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+
+
+    // 오름차순
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<i; j++) {
+            if (arr[i] != arr[j]) {
+                if (arr[i] > arr[j]) {
+                    length[0][i] = getMax(length[0][i], length[0][j]);
+                }
+            }
+        }
+        length[0][i]++;
+        max = getMax(max, length[0][i]);
+    }
+    // 내림차순
+    for (int i=n-1; i>=0; i--) {
+        for (int j=n-1; j>i; j--) {
+            if (arr[i] != arr[j]) {
+                if (arr[i] > arr[j]) {
+                    length[1][i] = getMax(length[1][i], length[1][j]);
+                }
+            }
+        }
+        length[1][i]++;
+        max = getMax(max, length[1][i]);
+    }
+
+    for (int i=0; i<n; i++) {
+        for (int j=i; j<n; j++) {
+            if (i==j) max = getMax(max, length[0][i] + length[1][j] - 1);
+            else if (arr[i] == arr[j] ) continue;
+            else max = getMax(max, length[0][i] + length[1][j]);
+        }
+    }
+
+    cout << max;
+
+    return 0;
+}
