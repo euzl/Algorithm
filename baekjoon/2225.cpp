7591d331864475f4521078af396e7668cb89229d
new Dean("유진") <euzl@khu.ac.kr>
2020-10-16 04:27:01 +0900

    2020/10/16 합분해 (improve)

--- baekjoon/2225.cpp
+++ baekjoon/2225.cpp
@@ -5,16 +5,14 @@ int main() {
     int n, k;
     scanf("%d %d", &n, &k);
 
-    long dp[2][201];
+    long dp[201] = {1, };
 
-    for (int i=0; i<=n; i++) dp[1][i] = 1;
-    for (int i=2; i<=k; i++) {
-        dp[i%2][0] = 1;
+    for (int i=1; i<=k; i++) {
         for (int j=1; j<=n; j++) {
-            dp[i%2][j] = (dp[i%2][j-1] + dp[(i+1)%2][j])%1000000000;
+            dp[j] = (dp[j-1] + dp[j])%1000000000;
         }
     }
-    printf("%ld", dp[k%2][n]);
+    printf("%ld", dp[n]);
 
     return 0;
 }
