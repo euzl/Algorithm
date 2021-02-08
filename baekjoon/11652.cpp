a8f6dd91a34db2d4fed04442e52da077fec83414
new Dean(  ) <euzl@khu.ac.kr>
2021-02-08 12:03:05 +0900

    2021/02/08 카드

---
+++ baekjoon/11652.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define SIZE 1000000
+
+long long deck[SIZE];
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    for (int i=0; i<n; i++) {
+        cin >> deck[i];
+    }
+    sort(deck, deck + n);
+
+    // deck[0] 을 처리하고 시작
+    int countTemp = 1;
+    int countMax = 1;
+    long long mostNum = deck[0];
+
+    // deck[1] 부터 ~
+    for (int i=1; i<n; i++) {
+        if (deck[i] == deck[i-1]) {
+            ++countTemp;
+            if (countTemp > countMax) {
+                countMax = countTemp;
+                mostNum = deck[i-1];
+            }
+        } else {
+            countTemp = 1;
+        }
+    }
+
+    cout << mostNum;
+
+    return 0;
+}
