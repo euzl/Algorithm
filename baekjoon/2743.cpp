1fa5b4c87580ef2f51a0abb963fb992875ddc02c
new Dean(  ) <euzl@khu.ac.kr>
2021-02-12 23:20:39 +0900

    2021/02/12 단어 길이 재기

---
+++ baekjoon/2743.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    string s;
+    cin >> s;
+    cout << s.length();
+    return 0;
+}
