3fa4e51f613d17dcbb09f21d1726cd893c5f8264
new Dean(  ) <euzl@khu.ac.kr>
2021-08-03 23:39:26 +0900

    2021/08/03 사칙연산 유효성 검사 (트리)

---
+++ swea/1233.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{	
+	static boolean answer = true;
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = 10;
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			answer = true;
+			String n = sc.nextLine();
+			int N = Integer.parseInt(n);
+			int [][] tree = new int[N+1][2]; // 1 left 2 right
+			char [] oper = new char[N+1]; // save operator
+			double [] num = new double[N+1]; // value of Node
+			
+			input(tree, num, oper, N, sc);
+			
+			System.out.printf("#%d %d\n", test_case, answer==true?1:0);
+		}
+	}
+	
+	static void input(int [][] tree, double [] num, char [] oper, int N, Scanner sc) {
+		int a;
+		String b;
+		int i;
+		String str;
+		
+		for (i=1; i<=N; i++) {
+			str = sc.nextLine(); // 한 줄씩 입력받기 
+			String [] arr = str.split(" "); // 띄어쓰기로 구분
+			
+			if (arr.length == 2) {
+				// 단말노드이므로 숫자여야만 함 
+				if (!isDigit(arr[1].charAt(0))) answer = false;
+			} else {
+				// 자식이 있는 경우
+				
+				// 연산자 여야 함.. 
+				if (isDigit(arr[1].charAt(0))) answer = false;
+				
+				// 숫자여야 함..
+				if (!isDigit(arr[2].charAt(0))) answer = false;
+				
+				if (arr.length == 4 && !isDigit(arr[3].charAt(0))) answer = false;
+			}
+		}
+	}
+	
+	static boolean isDigit(char t) {
+		if (t=='/' || t=='+'||t=='-'||t=='*') return false;
+		return true;
+	}
+}
