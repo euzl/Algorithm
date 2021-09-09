ded683324d51b154d9391ba8fbd606a26c256651
new Dean(  ) <euzl@khu.ac.kr>
2021-09-09 09:44:39 +0900

    2021/09/09 창용 마을 무리의 개수 (트리)

--- swea/7465.java
+++ swea/7465.java
@@ -1,72 +1,53 @@
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
-import java.io.FileInputStream;
-
-
+import java.util.*;
+import java.io.*;
+ 
 class Solution
 {
-	static int [][] graph = new int [101][101];
-	static int N, M;
-	static int [] visit = new int [101];
-	static int size;
-	
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			N = sc.nextInt();
-			M = sc.nextInt();
-			
-			for (int i=1; i<=N; i++) visit[i] = 0; // -1이면 무리에 속하지 않았음 
-			for (int i=1; i<=N; i++) {
-				for (int j=1; j<=N; j++) {
-					graph[i][j] = 0;
-				}
-			}
-			int a, b;
-			for (int i=0; i<M; i++) {
-				a = sc.nextInt();
-				b = sc.nextInt();
-				
-				graph[a][b] = 1;
-				graph[b][a] = 1;
-			}
-			
-			System.out.println("#"+test_case+" "+getCount());
-		}
-	}
-	
-	public static int getCount() {
-		int count = 0;
-		
-		for (int i=1; i<=N; i++) {
-			if (visit[i] == 0) {
-				makeGroup(i);
-				count++;
-			}
-		}
-		
-		return count;
-	}
-
-	// 아는 사람 싹 돌고 오
-	public static void makeGroup(int start) {
-		visit[start] = 1;
-		
-		for (int i=1; i<=N; i++) {
-			if (graph[start][i] == 1 && visit[i] == 0) {
-				makeGroup(i);
-			}
-		}
-	}
+    static int N, M;
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int T;
+        T=Integer.parseInt(br.readLine());
+ 
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+             
+            // 부모노드 저장 (p[x]==i 이면 각 트리의 root!)
+            int [] p = new int[N+1]; 
+            for (int i=0; i<=N; i++) p[i] = i;
+             
+            // 트리 만들기.
+            int a, b;
+            for (int i=0; i<M; i++) {
+                st = new StringTokenizer(br.readLine());
+                a = Integer.parseInt(st.nextToken());
+                b = Integer.parseInt(st.nextToken());
+ 
+                p[getRoot(p,b)] = getRoot(p,a);
+            }
+             
+            // 루트 개수 
+            int count=0;
+            for (int i=1; i<=N; i++) {
+                if (p[i] == i) count++;
+            }
+             
+            bw.write("#"+test_case+" "+count + "\n");
+            bw.flush();
+        }
+        bw.close();
+    }
+     
+    public static int getRoot(int [] p, int idx) {
+        while(true) {
+            if (p[idx] == idx) return idx;
+            idx = p[idx];
+        }
+    }
 }
