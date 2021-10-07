da6dd7c6f6edab3dbafa229951cda22cdd07308b
new Dean(  ) <euzl@khu.ac.kr>
2021-10-07 22:27:22 +0900

    2021/10/07 월간 코드 챌린지 10월

--- programmers/unknown/27710.java
+++ programmers/unknown/27710.java
@@ -1,3 +1,4 @@
+// 1번
 class Solution {
     public int solution(int n) {
         int answer = 0;
@@ -7,3 +8,38 @@ class Solution {
         return answer;
     }
 }
+
+// 2번 (70점)
+import java.util.*;
+
+class Solution {
+    int [] arr;
+    int size;
+    public int[] solution(int n, long left, long right) {
+        size = Math.toIntExact(right-left)+1;
+        makeArr(n, left);
+    
+        return arr;
+    }
+    
+    private void makeArr(int n, long start) {
+        arr = new int[size];
+        
+        int d = (int)(start/n);
+        long idx = d*n-1;
+        int k=0;
+        for (int i=d+1; i<=n; i++) {
+            for (int j=1; j<=n; j++) {
+                idx++;
+                if (idx < start) continue;
+                if (idx == start) k = -1;
+                k++;
+                if (k == size) return;
+                arr[k] = Math.max(i,j);
+            }
+        }
+    }
+}
+/**
+ * arr[r][c] = max(r, c)
+ */
